0f295268a699015be1d0b5702ce0edea7603a750
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-02-17 22:31:52 +0530

    pre-installation for render

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,6 +13,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
   console.log(err);
 });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -23,6 +27,12 @@ app.listen(3000, () => {
 
 app.use('/server/user', userRouter);
 app.use('/server/auth', authRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
